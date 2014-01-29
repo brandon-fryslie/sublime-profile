ac7a6f00f2b1ab9ff84f308c29e13f207d1fdfa8
Michael Byers <=>
2014-01-29 16:55:33 -0700

    Updated mousemap w/ GotoDefinition, OpenExtFile can now open Ext files :)

--- open_ext_file.py
+++ open_ext_file.py
@@ -6,8 +6,6 @@ class OpenExtFileCommand(sublime_plugin.TextCommand):
         window = sublime.active_window()
         view = self.view
 
-        base_path = os.path.expanduser('~/projects/appsdk/rui/src/')
-
         for region in view.sel():
 
             syntax = self.view.scope_name(region.begin())
@@ -16,11 +14,16 @@ class OpenExtFileCommand(sublime_plugin.TextCommand):
 
                 file_to_open = view.substr(view.extract_scope(region.begin())).strip('\'"')
 
-                file_to_open = re.sub('^Rally.', '', file_to_open)
+                if re.match('^Rally', file_to_open):
+                    base_path = os.path.expanduser('~/projects/appsdk/rui/src')
+                else:
+                    base_path = os.path.expanduser('~/projects/appsdk/lib/ext/4.2.2/src')
+
+                file_to_open = re.sub('^(Rally|Ext).', '', file_to_open)
 
                 file_to_open = re.sub('\.', '/', file_to_open) + '.js'
 
-                file_to_open = base_path + file_to_open
+                file_to_open = os.path.join(base_path, file_to_open)
 
                 if os.path.isfile(file_to_open):
 
